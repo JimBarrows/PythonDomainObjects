b8a2afd6891d38dcadb08f9fe3c203d37c572bb0
Jim Barrows <Jim.Barrows@gmail.com[color]>
2010-11-26 09:03:46 -0700

    Setup form works.

--- business/views.py
+++ business/views.py
@@ -1,27 +1,42 @@
-from django import forms
+from django.forms import ModelForm, DateField
 from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
 from party.models import Organization, PartyRole, PartyRoleType
+from common.widgets import DatePickerWidget
 
 def index( request):
 	''' does nothing '''
 
+primaryBusinessFormQuery =Organization.objects.filter( roles__description__exact='Internal Organization').filter( roles__description__exact='Parent Organization')
+
 def setup( request):
 	if request.method == 'POST':
-		form = Business(request.POST)
+		form = BusinessForm(request.POST)
 		if form.is_valid():
-			organization = Organization.objects.create( name=form.cleaned_data['name'])
-			internalOrgRole = PartyRoleType.objects.filter( description__contains='Internal Organization').get()
-			parentRole = PartyRoleType.objects.filter( description__contains='Parent Organization').get()
-			PartyRole.objects.create( party=organization, partyRoleType=internalOrgRole)
-			PartyRole.objects.create( party=organization, partyRoleType=parentRole )
+			if primaryBusinessFormQuery.exists():
+				organization = primaryBusinessFormQuery.get()
+				organization.name = form.cleaned_data['name']
+				organization.save()
+			else:
+				organization = Organization.objects.create( name=form.cleaned_data['name'])
+				internalOrgRole = PartyRoleType.objects.filter( description__contains='Internal Organization').get()
+				parentRole = PartyRoleType.objects.filter( description__contains='Parent Organization').get()
+				PartyRole.objects.create( party=organization, partyRoleType=internalOrgRole)
+				PartyRole.objects.create( party=organization, partyRoleType=parentRole )
 	else:
-		form = Business()
+		if( primaryBusinessFormQuery.exists()):
+			organization = primaryBusinessFormQuery.get()	
+			form = BusinessForm({ 'name' : organization.name})
+		else:
+			form = BusinessForm()
 	c = {'form':form}
 	c.update(csrf(request))
 	return render_to_response('business/setup/index.html', c)
 
-class Business( forms.Form ):
-	name = forms.CharField( max_length=100)
+class BusinessForm( ModelForm ):
+	dateStarted = DateField(label='Date Started', widget=DatePickerWidget)
+	class Meta:
+		model=Organization
+		fields=['name']
---
+++ common/widgets.py
@@ -0,0 +1,10 @@
+from django.forms import DateInput
+
+class DatePickerWidget(DateInput):
+    def __init__(self, **kwargs):
+        super(DateInput, self).__init__(attrs={"size":10, "class": "dateinput"}, **kwargs)
+
+    class Media:
+        css = {"all":("http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/themes/redmond/jquery-ui.css",)}
+        js = ("http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js",
+              "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js",)
