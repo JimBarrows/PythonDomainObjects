401bb3c5ac3f00d594e06f30b9479b96425d67f3
Jim Barrows <Jim.Barrows@gmail.com[color]>
2010-12-02 07:37:43 -0700

    Login required beginning.

--- urls.py
+++ urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 from views import index
+from django.contrib.auth.forms import AuthenticationForm
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^$', index),
     (r'^business/', include( 'business.urls')),
     (r'^products/', include( 'products.urls')),
 		(r'css/(?P<path>.*)$', 'django.views.static.serve', 
@@ -16,9 +18,9 @@ urlpatterns = patterns('',
 				{'document_root': '/home/jimbarrows/Desktop/bizondemand/js/'}),
 		(r'ckeditor/(?P<path>.*)$', 'django.views.static.serve', 
 				{'document_root': '/home/jimbarrows/Desktop/bizondemand/ckeditor/'}),
-    (r'^$', index),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^ckeditor/', include('ckeditor.urls')),
-
+		(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+		(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
 )
--- views.py
+++ views.py
@@ -8,10 +8,12 @@ from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render_to_response, redirect
 from django.core.context_processors import csrf
+from django.contrib.auth.decorators import login_required
 from party.models import Organization, PartyRole, PartyRoleType
 from common.widgets import DatePickerWidget
 from business.views import primaryBusinessFormQuery
 
+@login_required
 def index( request):
 	if primaryBusinessFormQuery.exists():
 		return render_to_response('index.html', {})
