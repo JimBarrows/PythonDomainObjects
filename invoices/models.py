68f4d5349c758f429859c76fd3b0081c408868a9
Jim Barrows <Jim.Barrows@gmail.com[color]>
2010-12-08 21:39:37 -0700

    Add Figure 7.1a

--- invoices/models.py
+++ invoices/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from polymorphic import PolymorphicModel
+from products.models import Product, Feature
 
-# Create your models here.
+class Invoice( models.Model ):
+	invoice_date = models.DateField()
+	message = models.TextField()
+	description = models.TextField()
+
+class InvoiceItem( models.Model ):
+	taxable = models.BooleanField()
+	quantity = models.IntegerField()
+	amount = models.DecimalField( max_digits = 8, decimal_places=2)
+	description = models.TextField()
+	invoice = models.ForeignKey( Invoice)
+	kind = models.ForeignKey( 'InvoiceItemType', blank=True, null=True)
+	product = models.ForeignKey( Product,  blank=True, null=True)
+	productFeature = models.ForeignKey( Feature, blank=True, null=True )
+
+class InvoiceItemType( models.Model ):
+	description = models.CharField( max_length=250 )
+
+
