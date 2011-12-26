586178783f8197b392c77c2cd2289e50dc1e5768
Jim Barrows <Jim.Barrows@gmail.com[color]>
2011-12-26 08:17:23 -0700

    removing the twitter stuff

--- settings.py
+++ settings.py
@@ -83,14 +83,11 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'bizondemand-python.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-		os.path.join(PROJECT_DIR, 'web/templates'),
-		os.path.join(PROJECT_DIR, 'web/css'),
-		os.path.join(PROJECT_DIR, 'web/js'),
-		os.path.join(PROJECT_DIR, 'web/img'),
-)
+    os.path.join(PROJECT_DIR, 'web/templates'),
+    os.path.join(PROJECT_DIR, 'web/css'),
+    os.path.join(PROJECT_DIR, 'web/js'),
+    os.path.join(PROJECT_DIR, 'web/img')
+    )
 
 INSTALLED_APPS = (
     'django.contrib.auth',
