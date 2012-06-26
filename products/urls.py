ba8a1e2f6cfa40ea8405ed901090a609ea3682a8
Jim Barrows <Jim.Barrows@gmail.com[color]>
2012-06-26 07:04:35 -0700

    Full CRUD on services

--- products/urls.py
+++ products/urls.py
@@ -1,14 +1,8 @@
 from django.conf.urls.defaults import *
 from views import *
 urlpatterns = patterns('products.views',
-#                       (r'^goods$',  good_index),
-#                       (r'^goods/(?P<good_id>\d+)/$',  good_edit),
-#                       (r'^goods/add$',  good_add),
-#                       (r'^goods/save$',  good_save),
-#                       (r'^goods/(?P<good_id>\d+)/save$',  good_update),
 	(r'^services$', login_required(ServicesList.as_view())),
 	(r'^services/add$', login_required(ServicesCreate.as_view())),
-#                       (r'^services/(?P<service_id>\d+)/$',  service_edit),
-#                       (r'^services/save$',  service_save),
-#                       (r'^services/(?P<service_id>\d+)/save$',  service_update),
+	(r'^services/(?P<pk>\d+)/$',  login_required(ServicesUpdate.as_view())),
+	(r'^services/delete/(?P<pk>\d+)/$',  login_required(ServicesDelete.as_view())),
                        )
