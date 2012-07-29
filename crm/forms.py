8efea37773015e2b9c0f099216355dffdfeb4e95
Jim Barrows <Jim.Barrows@gmail.com[color]>
2012-07-29 11:16:52 -0700

    Renamed everything to customer.  Customer add adds either a person or organization correctly.

--- crm/forms.py
+++ crm/forms.py
@@ -1,18 +1,20 @@
 from django import forms
 from django.forms import ModelForm
 from common.widgets import DatePickerWidget
-from party.models import Party
+from party.models import Party, OrganizationType
 
 customer_type_choices =(
 	('Person','Person'),
 	('Organization', 'Organization'),
 )
+
 class CustomerForm( ModelForm):
-	first_name = forms.CharField()
-	middle_name = forms.CharField()
-	last_name = forms.CharField()
-	name = forms.CharField()
-	customer_type = forms.ChoiceField( choices=customer_type_choices, widget=forms.RadioSelect)
+	first_name = forms.CharField(required=False)
+	middle_name = forms.CharField(required=False)
+	last_name = forms.CharField(required=False)
+	name = forms.CharField(required=False)
+	customer_type = forms.ChoiceField( choices=customer_type_choices, widget=forms.RadioSelect, required=False)
+	organization_type = forms.ModelChoiceField( queryset=OrganizationType.objects.all(), required=False)
 	class Meta:
 		model=Party
 		exclude=('classification', 'customer_type',)
--- crm/views.py
+++ crm/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from django.http import HttpResponseRedirect
 from models import *
-from party.models import Party, Person, PartyRole
+from party.models import Party, Person, PartyRole, Organization, PartyClassification
 from common.views import customer_role
 from crm.forms import CustomerForm
 
@@ -16,7 +16,18 @@ class CustomerCreate ( CreateView):
 	success_url='/crm'
 
 	def form_valid(self, form):
-		self.object = form.save()
+		print("Before self.object={0}".format(self.object))
+		if( form.cleaned_data['customer_type'] == 'Person') :
+			self.object = Person( first_name=form.cleaned_data['first_name'],
+														middle_name=form.cleaned_data['middle_name'],
+														last_name=form.cleaned_data['last_name'])
+			self.object.save()
+		else :
+			self.object = Organization(name=form.cleaned_data['name'])
+			self.object.save()
+			PartyClassification(party=self.object,
+													party_type = form.cleaned_data['organization_type']).save()
+		print("After self.object={0}".format(self.object))
 		add_roles( self.object)
 		return HttpResponseRedirect(self.get_success_url())
 
