03c9f9e5aa6846c3f2adfc31f8a4aae5f2f0fe67
Jim Barrows <Jim.Barrows@gmail.com>
2014-05-21 06:56:31 -0700

    Committing so I can change os's

--- party/models/Party.py
+++ party/models/Party.py
@@ -1,9 +1,6 @@
 from django.db import models
-from django.contrib.contenttypes.models import ContentType
 from polymodels.models import PolymorphicModel
 from datetime import datetime
-from common.models import DateRange
-import abc
 
 class Party(PolymorphicModel):
 	classification = models.ManyToManyField( 'PartyType', through='PartyClassification')
